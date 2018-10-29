3064d5ee5911a4808ce57654779327f1a7aebe78
Volodymyr Chernyshov <v.chernyshov@nullgr.com>
2018-10-29 11:18:12 +0200

    [JiraReport] Fix typo

--- runner.sh
+++ runner.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python3 jira_report.py --domain JIRA_DOMAIN --project JIRA_PROJECT_ID --email JIRA_USER_EMAIL --password JIRA_USER_PASSWORD --to EMAIL_RECEPIENT
+python3 jira_report.py --domain JIRA_DOMAIN --project JIRA_PROJECT_ID --email JIRA_USER_EMAIL --password JIRA_USER_PASSWORD --to EMAIL_RECIPIENT
